ca5384502febbd4bb69b95e1b0d79bfe5816f7f8
rahul-kumar-3 <rahulkumar2552006@gmail.com>
2024-09-28 17:55:36 +0530

    Update ThreeSumClosest.java

--- Arrays/ThreeSumClosest.java
+++ Arrays/ThreeSumClosest.java
@@ -1,4 +1,14 @@
 /*Leetcode Problem
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0)
 Time Complexity O(n3)*/
 //===========================================================================================================================================
 class Solution {
@@ -20,3 +30,29 @@ class Solution {
 }
 
 //====================================================================================================================================================
+/*Optimize code 
+Time complexity: O(N2)
+*/
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closestSum = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            int j = i+1;
+            int k = nums.length-1;
+            while(j < k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(Math.abs(target - closestSum) > Math.abs(target - sum)){
+                    closestSum = sum;
+                }
+                if(sum < target){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        return closestSum;
+    }
+}
