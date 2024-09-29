3966ef4049833c3cfc437bf0df69f8fe6b289508
rahul-kumar-3 <rahulkumar2552006@gmail.com>
2024-09-29 16:30:47 +0530

    Update FirstMissingPositive.java

--- Arrays/FirstMissingPositive.java
+++ Arrays/FirstMissingPositive.java
@@ -18,3 +18,19 @@ class Solution {
     }
 }
 //===========================================================================================================================================================
+/*Time Complexity O(n)*/
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Map<Integer, Boolean> map = new HashMap<>();
+        for(int n : nums){
+            if(n > 0){
+                map.put(n,true);
+            }
+        }
+        int posNo = 1;
+        while(true){
+            if(map.get(posNo) == null) return posNo;
+            posNo++;
+        }
+    }
+}
