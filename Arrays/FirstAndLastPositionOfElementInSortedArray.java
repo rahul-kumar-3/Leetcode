b8eb2fdb22912c5559d03627570b9dcae9b32379
rahul-kumar-3 <rahulkumar2552006@gmail.com>
2024-09-29 16:33:16 +0530

    Create FirstAndLastPositionOfElementInSortedArray.java

---
+++ Arrays/FirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,17 @@
+/*Leetcode Problem 
+First and Last Position of Element in Sorted Array
+*/
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+      int first = -1, last = -1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                if(first == -1){
+                    first = i;
+                }
+                last = i;
+            }
+        }
+        return new int[]{first, last};
+    }
+}
